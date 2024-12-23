0148189e6894720aa9b811db0220161123c1dcc2
Adal15 <121809334+Adal15@users.noreply.github.com>
2024-12-23 21:51:46 +0530

    Add files via upload

---
+++ Divisiblei.java
@@ -0,0 +1,9 @@
+package com.practice.programming;
+//To check wheather even number is divisible by both 3 and 11
+public class Divisiblei {
+   public static void main(String[] args) {
+	int n=33;
+	System.out.println(n%3==0 && n%11==0);
+	System.out.println("adal");
+}
+}
