f17b2291ec30213f610c77642e315b5ccd4dffcd
Adal15 <121809334+Adal15@users.noreply.github.com>
2025-01-06 01:02:56 +0530

    Add files via upload
    Armstrong Number :- sum of digits to the power of count of digits is same as given number.
    Ex :- 153=1*1*1+5*5*5+3*3*3

---
+++ Armstrong.java
@@ -0,0 +1,17 @@
+package com.practice.programming;
+
+public class Armstrong {
+	public static void main(String[] args) {
+		int n=13;
+		int temp=n;
+		int r,sum=0;
+		while(n>0) {
+			r=n%10;
+			n=n/10;
+			sum=sum+r*r*r;
+		}
+		if(temp==sum)
+			System.out.println("armstrongno");
+		
+	}
+}
