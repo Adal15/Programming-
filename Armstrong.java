248b1f781ea9dd56e3d22ef9a285837cb57c4b1a
Adal15 <121809334+Adal15@users.noreply.github.com>
2025-01-08 00:30:14 +0530

    Update Armstrong.java

--- Armstrong.java
+++ Armstrong.java
@@ -4,14 +4,15 @@ public class Armstrong {
 	public static void main(String[] args) {
 		int n=13;
 		int temp=n;
-		int r,sum=0;
+		int rem,sum=0;
 		while(n>0) {
-			r=n%10;
+			rem=n%10;
 			n=n/10;
 			sum=sum+r*r*r;
 		}
 		if(temp==sum)
-			System.out.println("armstrongno");
-		
+			System.out.println("armstrong number");
+		else
+			 System.out.println("Not an armstrong number");
 	}
 }
